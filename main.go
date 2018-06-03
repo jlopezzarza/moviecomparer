045baf81a9a06d3b91814baa368c78429677c624
jlopez <j.lopezarza@gmail.com>
2018-06-03 20:52:31 +0200

    Basic working api

---
+++ main.go
@@ -0,0 +1,82 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+)
+
+type movie struct {
+	ID          float64 `json:"id"`
+	Title       string  `json:"title"`
+	Releasedate string  `json:"release_date"`
+	Cast        []castmember
+}
+type castmember struct {
+	Order     float64 `json:"cast_id"`
+	Character string  `json:"character"`
+	ID        float64 `json:"id"`
+	Name      string  `json:"name"`
+}
+type moviesresults struct {
+	Total   float64 `json:"total_results"`
+	Results []movie `json:"results"`
+}
+type castresult struct {
+	ID   float64      `json:"id"`
+	Cast []castmember `json:"cast"`
+}
+
+func getdata(url string) (body []byte) {
+	resp, err := http.Get(url)
+	if err != nil {
+		fmt.Println(err)
+	}
+	body, _ = ioutil.ReadAll(resp.Body)
+	return
+}
+
+func (movie *movie) searchcast() {
+	url := fmt.Sprintf("https://api.themoviedb.org/3/movie/%f/credits?api_key=%s", movie.ID, os.Getenv("TMDB_KEY"))
+	body := getdata(url)
+	var data castresult
+	if err := json.Unmarshal(body, &data); err != nil {
+		fmt.Println("Search Cast - error with the json: ", err)
+	}
+	//reflect.ValueOf(&movie).Elem().FieldByName("Cast").Set()
+	movie.Cast = data.Cast
+	return
+}
+
+func searchmovies(w http.ResponseWriter, r *http.Request) {
+	//func searchmovies(searchpar string) {
+	searchpar := r.URL.Path[len("/searchmovies/"):]
+	if searchpar == "" {
+		fmt.Fprintf(w, "Empty query")
+		return
+	}
+	url := fmt.Sprintf("https://api.themoviedb.org/3/search/movie?api_key=%s&query=%s", os.Getenv("TMDB_KEY"), searchpar)
+	body := getdata(url)
+	var data moviesresults
+	if err := json.Unmarshal(body, &data); err != nil {
+		fmt.Println("Search movie - movieresults - json error: ", err)
+		return
+	}
+	for i := 0; i < len(data.Results); i++ {
+		data.Results[i].searchcast()
+	}
+	result, err := json.Marshal(&data)
+	if err != nil {
+		fmt.Println("Search movie - resulsts - json error: ", err)
+	}
+	fmt.Fprintf(w, string(result))
+	return
+}
+
+func main() {
+	http.HandleFunc("/searchmovies/", searchmovies)
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
