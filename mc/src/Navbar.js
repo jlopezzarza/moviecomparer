30b5e1bb257046ab8fd0754ece67faa40bb4aec4
Jorge Lopez <jorge.lopez@a3sec.com>
2019-03-06 20:41:07 +0100

    Add basic react with create-react-app

---
+++ mc/src/Navbar.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './Navbar.css';
+
+class Navbar extends Component {
+    render() {
+        return(
+            <div className="Navbar">
+            <ul>
+                <li>Movie Comparer</li>
+            </ul>
+            </div>
+        )
+    }
+}
+
+export default Navbar
