9420ca64c7f1895b81f9c17822a203792ca416e9
Jorge Lopez <jorge.lopez@a3sec.com>
2019-03-10 20:58:06 +0100

    Reupdate files format

--- mc/src/App.js
+++ mc/src/App.js
@@ -7,70 +7,70 @@ import SearchBar from './components/board/SearchBar';
 import axios from 'axios';
 
 class App extends Component {
-  state = {
-    param: null,
-    cards: []
-  }
+    state = {
+        param: null,
+        cards: []
+    }
 
-  searchMovies = (e) => {
-    e.preventDefault();
-    if (this.state.param != null) {
-      axios.get(encodeURI("http://localhost:8080/searchmovies/" + this.state.param))
-        .then(res => {
-          if (res.data.results.length > 0) {
-            let count = this.state.cards.length + 1
-            let card = {
-              id: count,
-              movieresults: res.data.results,
-              loadmovies: true,
-              loadinfo: false
-            };
-            this.setState({
-              ...this.state,
-              cards: [...this.state.cards, card],
-              cardscount: count
-            });
-          }
-        })
-        .catch((error) => {
-          alert("Oops! There was a problem with the search, try again later");
+    searchMovies = (e) => {
+        e.preventDefault();
+        if (this.state.param != null) {
+            axios.get(encodeURI("http://localhost:8080/searchmovies/" + this.state.param))
+                .then(res => {
+                    if (res.data.results.length > 0) {
+                        let count = this.state.cards.length + 1
+                        let card = {
+                            id: count,
+                            movieresults: res.data.results,
+                            loadmovies: true,
+                            loadinfo: false
+                        };
+                        this.setState({
+                            ...this.state,
+                            cards: [...this.state.cards, card],
+                            cardscount: count
+                        });
+                    }
+                })
+                .catch((error) => {
+                    alert("Oops! There was a problem with the search, try again later");
+                })
+        }
+    }
+    saveParam = (e) => {
+        this.setState({
+            param: e.target.value
         })
     }
-  }
-  saveParam = (e) => {
-    this.setState({
-      param: e.target.value
-    })
-  }
-  searchMovieInfo = (movieid, cardid) => {
-    axios.get(encodeURI("http://localhost:8080/movieinfo/" + movieid))
-      .then(res => {
-        let oldCards = this.state.cards.filter(card => { return card.id !== cardid });
-        let newCard = this.state.cards.filter(card => { return card.id === cardid });
-        newCard.loadinfo = true;
-        newCard.loadmovies = false;
-        newCard.movieinfo = res.data;
-        this.setState({
-          ...this.state,
-          cards: [...oldCards, newCard]
-        })
-      })
-      .catch((error) => {
-        alert("Oops! There was a problem with the search, try again later");
-      })
-  }
+    searchMovieInfo = (movieid, cardid) => {
+        axios.get(encodeURI("http://localhost:8080/movieinfo/" + movieid))
+            .then(res => {
+                let oldCards = this.state.cards.filter(card => { return card.id !== cardid });
+                let newCard = this.state.cards.filter(card => { return card.id === cardid });
+                newCard.loadinfo = true;
+                newCard.loadmovies = false;
+                newCard.movieinfo = res.data;
+                this.setState({
+                    ...this.state,
+                    cards: [...oldCards, newCard]
+                })
+            })
+            .catch((error) => {
+                alert("Oops! There was a problem with the search, try again later");
+            })
+    }
 
-  render() {
-    return (
-      <div className="">
-        <Navbar />
-        <div className="container">
-          <SearchBar searchMovies={this.searchMovies} saveParam={this.saveParam} />
-        </div>
-        <CardBoard cards={this.state.cards} cardscount={this.state.cardscount} searchMovieInfo={this.searchMovieInfo} />
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className="">
+                <Navbar />
+                <div className="container">
+                    <SearchBar searchMovies={this.searchMovies} saveParam={this.saveParam} />
+                </div>
+                <CardBoard cards={this.state.cards} cardscount={this.state.cardscount} searchMovieInfo={this.searchMovieInfo} />
+            </div>
+        );
+    }
 }
 
 export default App;
