92382a2d104a3a2dc896570ce6cfeda6a54de3b9
Jorge Lopez <jorge.lopez@a3sec.com>
2019-03-12 18:10:05 +0100

    Update logos and add it to navbar

--- mc/src/components/Navbar.js
+++ mc/src/components/Navbar.js
@@ -1,10 +1,15 @@
 import React from 'react'
+import logo from '../images/logo.png'
 
 const Navbar = () => {
     return (
         <div className="navbar-fixed">
             <nav className="nav-wrapper center-align teal lighten-2">
-                <h5>MovieComparer</h5>
+                <div className="center"><ul className="">
+                <li><img src={logo} alt="not found" className="logo"></img></li>
+                <li><a href="#" class="brand-logo"><h5>Movie Comparer</h5></a></li>
+                </ul>
+                </div>
             </nav>
         </div>
     )
